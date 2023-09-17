78825d7ca8fd3e941e617192a0b0198864e6822b
sudoku-jm <sudoku8386@gmail.com>
2023-09-17 13:53:31 +0900

    20230917 진행 잠시 중단

--- back/app.js
+++ back/app.js
@@ -35,7 +35,7 @@ passportConfig();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","http://192.168.1.128:13000", "http://192.168.1.128:23000"],
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
